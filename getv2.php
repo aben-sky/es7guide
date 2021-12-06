de47057c6ebebc0de2245528cf60b47aac4e1c14
aben <xuyuefang1998@163.com>
2021-12-06 15:12:45 +0800

    2.x文档: 修正移动端适配

--- getv2.php
+++ getv2.php
@@ -893,23 +893,37 @@ function convertToLocalFile($link) {
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>$title</title>
+    <!-- Give IE8 a fighting chance -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 	<link rel="stylesheet" type="text/css" href="styles.css" />
 </head>
 <body>
-<div style="color:gray;">原文地址: https://www.elastic.co/guide/cn/elasticsearch/guide/current/$link</div>
-<section id="guide" lang="zh_cn">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-md-8 guide-section">
-            $body
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4" id="right_col">
-            $docList
-            </div>
+<div class="main-container">
+    <section id="content">
+        
+        <div class="content-wrapper">
+            <section id="guide" lang="zh_cn">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-md-8 guide-section">
+                            <div style="color:gray; word-break: break-all; font-size:12px;">原文地址: https://www.elastic.co/guide/cn/elasticsearch/guide/current/$link</div>
+                        $body
+                        </div>
+                        <div class="col-xs-12 col-sm-4 col-md-4" id="right_col">
+                        $docList
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-    </div>
-</section>
+    </section>
+</div>
 <script src="js.js"></script>
 </body>
 </html>
@@ -950,5 +964,5 @@ function convertToLocalFiles() {
 //getOriginalHtmls();
 
 //原文档转换到本地文档
-//convertToLocalFile('_full_text_search.html');//单个转换测试
+//convertToLocalFile('inverted-index.html');//单个转换测试
 convertToLocalFiles();

e198f8a45cd603110adad09fdbcb142c420c31c3
aben <xuyuefang1998@163.com>
2021-12-22 11:11:35 +0800

    7.7英文文档: 补充图片的抓取

--- getv7.7.php
+++ getv7.7.php
@@ -6,6 +6,8 @@ require_once 'vendor/autoload.php';
 
 use QL\QueryList;
 
+$folderPath = '7.7en/origin/';
+
 function tepGetDocList() {
     //所有文档列表
     $docList = file_get_contents('doc_list_7.7_en.txt');
@@ -17,6 +19,7 @@ function tepGetDocList() {
  * 从官网抓取文档, 并存入本地
  */
 function getOriginalHtmls() {
+    global $folderPath;
     $docList = tepGetDocList();
     $arrLink = QueryList::getInstance()
         ->setHtml($docList)
@@ -32,7 +35,7 @@ function getOriginalHtmls() {
     $total = count($arrLink);
     $seq = 1;
     foreach ($arrLink AS $link) {
-        if (is_file('7.7en/origin/' . $link)) {
+        if (is_file($folderPath . $link)) {
             echo $link . ' 已存在, 忽略' . PHP_EOL;
             $seq++;
             continue;
@@ -83,8 +86,9 @@ function getOriginalHtml($link) {
     if (empty($originHtml)) {
         return false;
     }
+    global $folderPath;
     //写入
-    file_put_contents('7.7en/origin/' . $link, $originHtml);
+    file_put_contents($folderPath . $link, $originHtml);
     return true;
 }
 
@@ -92,7 +96,8 @@ function getOriginalHtml($link) {
  * 把官网的文档转换到本地
  */
 function convertToLocalFile($link) {
-    $html = file_get_contents('7.7en/origin/' . $link);
+    global $folderPath;
+    $html = file_get_contents($folderPath . $link);
     $document = QueryList::getInstance()->setHtml($html);
 
     //获取title
@@ -166,8 +171,77 @@ function convertToLocalFile($link) {
 </html>
 EOF;
 
-    //写入
-    file_put_contents('html/7.7en/' . $link, $html);
+    //提取图片并保存
+    $arrImages = [];
+    //$document->find('img')->map(function ($img) use ($arrImages) {
+    //    //也可以直接在这里处理, 但是无法修改外部的html了
+    //});
+    foreach ($document->find('img')->attrs('src')->all() AS $img) {
+        $arrImages[] = $img;
+    }
+    $retry = 10;//最多尝试10次
+
+    $client = new \GuzzleHttp\Client();
+    foreach ($arrImages AS $src) {
+        //处理完整路径的图片
+        if (preg_match('/^http(s)?:\/\//', $src)) {
+            $url = $src;
+            echo $url . PHP_EOL;
+            $arr = parse_url($src);
+            if ($arr === false) {
+                echo 'ERROR: ' . $src . ' 解析失败';
+                file_put_contents('getv7.7.log', date('Y-m-d H:i:s') . PHP_EOL . $link . ' => ' . $src . ' 解析失败', FILE_APPEND);
+                break;
+            }
+            $path = $arr['host'] . $arr['path'];
+            $imgSavePath = __DIR__ . '/html/7.7en/images/' . $path;
+            //替换html
+            $html = str_replace('src="' . $src . '"', 'src="images/' . $path . '"', $html);
+        } else {
+            $url = 'https://www.elastic.co/guide/en/elasticsearch/reference/7.7/' . $src;
+            $imgSavePath = __DIR__ . '/html/7.7en/' . $src;
+        }
+        echo $url . ' => ';
+        echo $imgSavePath . ' : ';
+
+        if (!is_file($imgSavePath)) {
+            //创建文件夹
+            $pathInfo = pathinfo($imgSavePath);
+            $dir = $pathInfo['dirname'];
+            if (!is_dir($dir)) {
+                echo '创建目录: ' . $dir . PHP_EOL;
+                mkdir($dir, 0777, true);
+            }
+
+            echo '下载中....';
+            $rt = false;
+            for ($i = 1; $i < $retry; $i++) {
+                try {
+                    $rt = $client->request('GET', $url, ['timeout' => 10]);
+                    if ($rt->getStatusCode() == 200) {
+                        echo ' 成功';
+                        file_put_contents($imgSavePath, $rt->getBody());
+                        break;
+                    } else {
+                        echo ' 尝试' . $i;
+                        sleep(2);
+                    }
+                } catch (\Exception $ex) {
+                    echo ' 尝试' . $i;
+                    sleep(2);
+                }
+            }
+            if ($rt === false) {
+                echo '下载失败';
+            }
+        } else {
+            echo ' 存在, 忽略..';
+        }
+        echo PHP_EOL;
+    }
+
+    //写入html
+    file_put_contents(__DIR__ . '/html/7.7en/' . $link, $html);
 
     return true;
 }
@@ -201,5 +275,6 @@ function convertToLocalFiles() {
 //getOriginalHtmls();
 
 //原文档转换到本地文档
-//convertToLocalFile('action-conditions.html');//单个转换测试
+//convertToLocalFile('actions-pagerduty.html');//单个转换测试
+//convertToLocalFile('query-dsl-function-score-query.html');//单个转换测试: 带完整地址的图片
 convertToLocalFiles();
